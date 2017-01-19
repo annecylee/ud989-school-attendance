66e30f743561f076a26e73f104fd0d3838126d77
annecylee <annecylee@gmail.com>
2017-01-19 13:05:38 -0800

    Bugs fixed

--- js/app.js
+++ js/app.js
@@ -5,13 +5,12 @@
  */
 
 var model = {
-  schoolDay: 12,
   students: [
-    {name: 'Slappy the Frog', missedDay: 5},
-    {name: 'Lilly the Lizard', missedDay: 3},
-    {name: 'Paulrus the Walrus', missedDay: 6},
-    {name: 'Gregory the Goat', missedDay: 6},
-    {name: 'Adam the Anaconda', missedDay: 8}
+    {name: 'Slappy the Frog', missedDay: 12},
+    {name: 'Lilly the Lizard', missedDay: 12},
+    {name: 'Paulrus the Walrus', missedDay: 12},
+    {name: 'Gregory the Goat', missedDay: 12},
+    {name: 'Adam the Anaconda', missedDay: 12}
   ]
 }
 
@@ -24,12 +23,32 @@ var octupos = {
   getStudents: function(){
     return model.students
   },
-};
 
+  decreaseMissedDay: function(student){
+    student.missedDay -= 1;
+    dayMissedView.render()
+  }
+};
 
 //show days missed
 var dayMissedView = {
+
   init: function(){
+    var students = octupos.getStudents();
+    var studentsAttendence = $('tr.student')
+    for(i = 0; i < studentsAttendence.length; i++){
+      var student = students[i];
+      $('#' + studentsAttendence[i].id + ' input').change((function(student){
+        return function(){
+          if(this.checked){
+            octupos.decreaseMissedDay(student)
+          }
+        }
+      })(student));
+    };
+  },
+
+  render: function(){
     var students = octupos.getStudents();
     var elem = $('td.missed-col')
     var i
@@ -38,40 +57,7 @@ var dayMissedView = {
       // constructs a new jQuery object from one element within index
       elem.eq(i).html(missedDay)
     };
-
-    var studentsAttendence = $('tr.student')
-    var a = $('#' + studentsAttendence[0].id + ' input')
-    console.log(a);
-  },
-
-  render: function(){
-    var students = octupos.getStudents();
-    var studentsAttendence = $('tr.student')
-    for(i = 0; i < studentsAttendence.length; i++){
-      var student = students[i];
-      $('#' + studentsAttendence[i].id + ' input').change((function(){
-        console.log(student);
-        return function(student){
-          if(this.checked){
-            console.log(student);
-            student.missedDay -= 1
-          }
-        }
-      })(student))
-    };
   }
 };
-
-//show days attended
-var checkboxVeiw = {
-  init: function(){
-    //get students row elements
-    var elem = $('.student');
-  },
-
-  render: function(){
-
-  }
-}
 
 octupos.init()
